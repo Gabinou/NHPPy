2df29745e7653695195a085438c62ddbce644062
Gabriel Taillon <ga.taillon@gmail.com>
2018-10-05 02:11:13 +0900

    Added a bare-bones version of the thinning algorithm, with basic structure. Algorithm is untested.

--- stochastic/continuous/spatial_point_process.py
+++ stochastic/continuous/spatial_point_process.py
@@ -3,6 +3,7 @@
 from stochastic.continuous.poisson import PoissonProcess
 import inspect
 import numpy as np
+import scipy
 
 class SpatialPointProcess(PoissonProcess):
     r"""Spatial Point Process.
@@ -24,9 +25,9 @@ class SpatialPointProcess(PoissonProcess):
     :param dict rate_kwargs: keyword args for callable ``density``
     """
 
-    def __init__(self, density, density_args={}, density_kwargs={}):
+    def __init__(self, density, density_args=(), density_kwargs={}):
         self.density = density
-        self.bounds = bounds
+        self.density_args = density_args
         self.density_kwargs = density_kwargs
 
     def __str__(self):
@@ -57,24 +58,61 @@ class SpatialPointProcess(PoissonProcess):
         Delimits the point generation space. """
         return self._density_args
 
-    @bounds.setter
+    @density_args.setter
     def density_args(self, value):
         if not isinstance(value, (list, tuple)):
             raise ValueError("Density args must be a list, tuple.")
-        self._bounds = value
+        self._density_args = value
 
     @property
     def density_kwargs(self):
         """Keyword arguments for the density function."""
         return self._density_kwargs
 
-    @rate_kwargs.setter
+    @density_kwargs.setter
     def density_kwargs(self, value):
         if not isinstance(value, dict):
             raise ValueError("Density kwargs must be a dict.")
         self._density_kwargs = value
 
-    def sample(self, bounds=(), n=None, algo='thinning'):
+    def _sample_spatial_point_process(self, n=None, bounds=(), algo='thinning', blocksize=1000):
+        if (n is not None) & (bounds is not ()):
+            if algo=='thinning':
+                if callable(self.density):
+                    boundstuple=[]
+                    for i in bounds: boundstuple+=(tuple(i),)
+                    max = scipy.optimize.minimize(lambda x: -self.density(*x),x0=[np.mean(i) for i in bounds],bounds = boundstuple)
+                    lmax = self.density(*max.x, *self.density_args, *self.density_kwargs)
+                else:
+                    lmax=np.amax(density)
+                Thinned=[]
+                while len(Thinned) < n:
+                    for i in bounds:
+                        if 'Unthin' not in locals():
+                            Unthin=np.random.uniform(*i, size=(blocksize))
+                        else:
+                            Unthin=np.vstack((Unthin, np.random.uniform(*i, size=(blocksize))))
+                    Unthin.T
+                    if len(Unthin.shape)==1:
+                        Unthin = np.reshape(Unthin, (1, len(Unthin)))
+                    U = np.random.uniform(size=(blocksize))
+                    if callable(self.density): 
+                        Criteria = self.density(*Unthin)/lmax
+                    else:
+                        Criteria2D = self.density/lmax
+                        Indx = (Unthinx*density.shape[0]).astype(int)
+                        Indy = (Unthiny*density.shape[1]).astype(int)
+                        Criteria = Criteria2D[Indx, Indy]
+                        Unthin = np.transpose(np.vstack((Unthinx, Unthiny)))
+                    if Thinned == []: 
+                        Thinned = Unthin.T[U < Criteria, :]
+                    else:
+                        Thinned = np.vstack((Thinned, Unthin.T[U < Criteria, :]))
+                    del Unthin
+                Thinned = Thinned[:n, :]
+                return(Thinned)
+
+    def sample(self, n=None, bounds=(), algo='thinning', blocksize=1000):
         """Generate a realization.
 
         The number of samples ``n`` and the spatial ``bounds`` must be provided.
@@ -83,41 +121,7 @@ class SpatialPointProcess(PoissonProcess):
 
         :param int n: the number of arrivals to simulate
         :param tuple bounds: the bounds in which to generate arrivals
+        :param str algo: the general algorithm
         """
-        if callable(lambdaa):
-            boundstuple=[]
-            for i in Boundaries: boundstuple+=(tuple(i),)
-            max = scipy.optimize.minimize(lambda x: -lambdaa(*x),x0=[np.mean(i) for i in Boundaries],bounds = boundstuple)
-            lmax=lambdaa(*max.x)
-        else:
-            lmax=np.amax(lambdaa)
-        Thinned=[]
-        while len(Thinned)<Samples:
-            for i in Boundaries:
-                if 'Unthin' not in locals():
-                    Unthin=np.random.uniform(*i,size=(blocksize))
-                else:
-                    Unthin=np.vstack((Unthin,np.random.uniform(*i,size=(blocksize))))
-            Unthin.T
-            if len(Unthin.shape)==1:
-                Unthin=np.reshape(Unthin,(1,len(Unthin)))
-            U=np.random.uniform(size=(blocksize))
-            if callable(lambdaa): 
-                Criteria=lambdaa(*Unthin)/lmax
-            else:
-                Criteria2D=lambdaa/lmax
-                Indx=(Unthinx*lambdaa.shape[0]).astype(int)
-                Indy=(Unthiny*lambdaa.shape[1]).astype(int)
-                Criteria=Criteria2D[Indx,Indy]
-                Unthin=np.transpose(np.vstack((Unthinx,Unthiny)))
-            if Thinned==[]: 
-                Thinned=Unthin.T[U<Criteria,:]
-            else:
-                Thinned=np.vstack((Thinned,Unthin.T[U<Criteria,:]))
-            del Unthin
-        Thinned=Thinned[:Samples,:]
-        return(Thinned)
 
-
-        self.rate = self._sample_rate()
-        return self._sample_poisson_process(n, length, zero)
+        return self._sample_spatial_point_process(n, bounds, algo, blocksize)
