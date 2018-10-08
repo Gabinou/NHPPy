cdc4903b64c924c01af89a499b6f7a90fdc52322
Gabriel Taillon <ga.taillon@gmail.com>
2018-10-08 17:37:07 +0900

    This version works with n-dim density arrays, and functions, with the thinning algo. Considering changing the algo to better separate callabe/matrix

--- stochastic/continuous/spatial_point_process.py
+++ stochastic/continuous/spatial_point_process.py
@@ -19,15 +19,16 @@ class SpatialPointProcess(PoissonProcess):
     variables with mean :math:`1/\lambda`. Use the ``rate`` attribute to get
     the most recently generated random rate.
 
-    :param callable density: either a callable or n-dimensional array
-    :param tuple bounds: only positinal argument of ``density`` if it is
-    callable
-    :param dict rate_kwargs: keyword args for callable ``density``
+    :param callable density: either a callable or n-dimensional array. Note:
+    Only a single positional argument is assumed, a n-dimensinoal array.
+    :param tuple bounds: Bounds in wihch to simulate samples. The number of 
+    bound pairs must be the same as input arguments of callable density or 
+    number of the density array.
+    :param dict density_kwargs: keyword args for callable ``density``
     """
 
-    def __init__(self, density, density_args=(), density_kwargs={}):
+    def __init__(self, density, density_kwargs={}):
         self.density = density
-        self.density_args = density_args
         self.density_kwargs = density_kwargs
 
     def __str__(self):
@@ -35,10 +36,9 @@ class SpatialPointProcess(PoissonProcess):
 
     def __repr__(self):
         return "MixedPoissonProcess(" \
-            "density={d}, density_args={da}, density_kwargs={dkw})".format(
+            "density={d}, density_kwargs={dkw})".format(
                 rf=str(self.density),
-                ra=str(self.density_args),
-                rkw=str(self.density_akwargs)
+                rkw=str(self.density_kwargs)
             )
 
     @property
@@ -51,18 +51,6 @@ class SpatialPointProcess(PoissonProcess):
         if (not callable(value)) & (not isinstance(value, (list, tuple, np.ndarray))):
             raise ValueError("Density must be a callable or n-dimensional array.")
         self._density = value
-
-    @property
-    def density_args(self):
-        """Positional arguments for the density function. 
-        Delimits the point generation space. """
-        return self._density_args
-
-    @density_args.setter
-    def density_args(self, value):
-        if not isinstance(value, (list, tuple)):
-            raise ValueError("Density args must be a list, tuple.")
-        self._density_args = value
 
     @property
     def density_kwargs(self):
@@ -78,39 +66,46 @@ class SpatialPointProcess(PoissonProcess):
     def _sample_spatial_point_process(self, n=None, bounds=(), algo='thinning', blocksize=1000):
         if (n is not None) & (bounds is not ()):
             if algo=='thinning':
+
                 if callable(self.density):
                     boundstuple=[]
                     for i in bounds: boundstuple+=(tuple(i),)
-                    max = scipy.optimize.minimize(lambda x: -self.density(*x),x0=[np.mean(i) for i in bounds],bounds = boundstuple)
-                    lmax = self.density(*max.x, *self.density_args, *self.density_kwargs)
+                    max = scipy.optimize.minimize(lambda x: -self.density(x),x0=[np.mean(i) for i in bounds],bounds = boundstuple)
+                    lmax = self.density(max.x, *self.density_kwargs)
                 else:
-                    lmax=np.amax(density)
-                Thinned=[]
-                while len(Thinned) < n:
-                    for i in bounds:
-                        if 'Unthin' not in locals():
-                            Unthin=np.random.uniform(*i, size=(blocksize))
-                        else:
-                            Unthin=np.vstack((Unthin, np.random.uniform(*i, size=(blocksize))))
-                    Unthin.T
-                    if len(Unthin.shape)==1:
-                        Unthin = np.reshape(Unthin, (1, len(Unthin)))
+                    lmax = np.amax(self.density)
+
+                Thinned = np.array([])
+                while len(Thinned.T) < n:
+                    if callable(self.density):
+                        for bound in bounds:
+                            if 'Unthinned' not in locals():
+                                Unthinned = np.random.uniform(*bound, size=(blocksize))
+                            else:
+                                Unthinned = np.vstack((Unthinned, np.random.uniform(*bound, size=(blocksize))))
+                    else:
+                        for shape in self.density.shape:
+                            if 'Unthinned' not in locals():
+                                Unthinned = np.random.randint(0, shape, size=(blocksize))
+                            else:
+                                Unthinned = np.vstack((Unthinned, np.random.randint(0, shape, size=(blocksize))))
+                    if len(Unthinned.T.shape)==1:
+                        Unthinned.T = np.reshape(Unthinned, (1, len(Unthinned)))
                     U = np.random.uniform(size=(blocksize))
                     if callable(self.density): 
-                        Criteria = self.density(*Unthin)/lmax
+                        Criteria = self.density(Unthinned.T)/lmax
                     else:
-                        Criteria2D = self.density/lmax
-                        Indx = (Unthinx*density.shape[0]).astype(int)
-                        Indy = (Unthiny*density.shape[1]).astype(int)
-                        Criteria = Criteria2D[Indx, Indy]
-                        Unthin = np.transpose(np.vstack((Unthinx, Unthiny)))
-                    if Thinned == []: 
-                        Thinned = Unthin.T[U < Criteria, :]
+                        Criteria_ndim = self.density/lmax
+                        Criteria = []
+                        for point in Unthinned.T:
+                            Criteria.append(Criteria_ndim[tuple(point)])
+                    if len(Thinned) == 0:
+                        Thinned = Unthinned[:, U < Criteria]
                     else:
-                        Thinned = np.vstack((Thinned, Unthin.T[U < Criteria, :]))
-                    del Unthin
-                Thinned = Thinned[:n, :]
-                return(Thinned)
+                        Thinned = np.hstack((Thinned, Unthinned[:, U < Criteria]))
+                    del Unthinned
+                Thinned = Thinned[:, :n]
+                return(Thinned.T)
 
     def sample(self, n=None, bounds=(), algo='thinning', blocksize=1000):
         """Generate a realization.
@@ -123,5 +118,11 @@ class SpatialPointProcess(PoissonProcess):
         :param tuple bounds: the bounds in which to generate arrivals
         :param str algo: the general algorithm
         """
-
+        print('a')
+        if callable(self.density):
+            try:
+                print(*bounds)
+                self.density(*bounds, **self.density_kwargs)
+            except:
+                pass
         return self._sample_spatial_point_process(n, bounds, algo, blocksize)
