8acc42bd9492eb5cf631f848eddd782c30bc83d9
Gabriel Taillon <ga.taillon@gmail.com>
2018-10-09 14:19:25 +0900

    Added an exception to catch dimension mismatch in the case of callable density.

--- stochastic/continuous/spatial_point_process.py
+++ stochastic/continuous/spatial_point_process.py
@@ -4,6 +4,7 @@ from stochastic.continuous.poisson import PoissonProcess
 import numpy as np
 import scipy
 
+
 class SpatialPointProcess(PoissonProcess):
     r"""Spatial Point Process.
 
@@ -20,8 +21,8 @@ class SpatialPointProcess(PoissonProcess):
 
     :param callable density: either a callable or n-dimensional array. Note:
     Only a single positional argument is assumed, a n-dimensinoal array.
-    :param tuple bounds: Bounds in wihch to simulate samples. The number of 
-    bound pairs must be the same as input arguments of callable density or 
+    :param tuple bounds: Bounds in wihch to simulate samples. The number of
+    bound pairs must be the same as input arguments of callable density or
     number of the density array.
     :param dict density_kwargs: keyword args for callable ``density``
     """
@@ -31,14 +32,14 @@ class SpatialPointProcess(PoissonProcess):
         self.density_kwargs = density_kwargs
 
     def __str__(self):
-        return "Spatial point process with specified density function in multidimensional space."
+        return "Spatial point process with specified density function\
+                in multidimensional space."
 
     def __repr__(self):
         return "MixedPoissonProcess(" \
             "density={d}, density_kwargs={dkw})".format(
                 rf=str(self.density),
-                rkw=str(self.density_kwargs)
-            )
+                rkw=str(self.density_kwargs))
 
     @property
     def density(self):
@@ -47,8 +48,10 @@ class SpatialPointProcess(PoissonProcess):
 
     @density.setter
     def density(self, value):
-        if (not callable(value)) & (not isinstance(value, (list, tuple, np.ndarray))):
-            raise ValueError("Density must be a callable or n-dimensional array, list or tuple.")
+        if ((not callable(value)) &
+           (not isinstance(value, (list, tuple, np.ndarray)))):
+            raise ValueError("Density must be a callable or\
+                              n-dimensional array, list or tuple.")
         self._density = value
 
     @property
@@ -62,23 +65,35 @@ class SpatialPointProcess(PoissonProcess):
             raise ValueError("Density kwargs must be a dict.")
         self._density_kwargs = value
 
-    def _sample_spatial_point_process(self, n=None, bounds=(), algo='thinning', blocksize=1000):
+    def _sample_spatial_point_process(self, n=None, bounds=(),
+                                      algo='thinning', blocksize=1000):
         if (n is not None) & (bounds is not ()):
-            Thinned = np.empty((1,len(bounds)))
+            Thinned = np.empty((1, len(bounds)))
             if algo == 'thinning':
                 if callable(self.density):
-                    boundstuple=[]
-                    for i in bounds: boundstuple+=(tuple(i),)
-                    max = scipy.optimize.minimize(lambda x: -self.density(x),x0=[np.mean(i) for i in bounds],bounds = boundstuple)
-                    density_max = self.density(max.x, *self.density_kwargs)           
+                    try:
+                        self.density(range(len(bounds)), **self.density_kwargs)
+                    except IndexError:
+                        raise ValueError("Number of dimensions of bounds must\
+                                          match density function")
+                    boundstuple = []
+                    for i in bounds: boundstuple += (tuple(i),)
+                    max = scipy.optimize.minimize(lambda x: -self.density(x),
+                                                  x0=[np.mean(i) for i in bounds],
+                                                  bounds=boundstuple)
+                    density_max = self.density(max.x, *self.density_kwargs)
                     while len(Thinned) < n:
                         Unthinned = np.empty(blocksize)
                         for bound in bounds:
-                            Unthinned = np.vstack((Unthinned, np.random.uniform(*bound, size=(blocksize))))
+                            Unthinned = np.vstack((Unthinned,
+                                                  np.random.uniform(*bound,
+                                                  size=(blocksize)))
+                                                  )
                         Unthinned = Unthinned[1:, :]
                         U = np.random.uniform(size=(blocksize))
                         Criteria = self.density(Unthinned)/density_max
-                        Thinned = np.vstack((Thinned, Unthinned[:, U < Criteria].T))
+                        Thinned = np.vstack((Thinned,
+                                             Unthinned[:, U < Criteria].T))
                 elif isinstance(self.density, (list, tuple, np.ndarray)):
                     if len(self.density.shape) != len(bounds):
                         raise ValueError(
@@ -88,14 +103,18 @@ class SpatialPointProcess(PoissonProcess):
                         Unthinned = np.empty(blocksize, dtype='int')
                         # int: the outputs are indexes of the density array.
                         for shape in self.density.shape:
-                            Unthinned = np.vstack((Unthinned, np.random.randint(0, shape, size=(blocksize))))
+                            Unthinned = np.vstack((Unthinned,
+                                                  np.random.randint(0,
+                                                                    shape,
+                                                                    size=(blocksize))))
                         Unthinned = Unthinned[1:, :]
                         U = np.random.uniform(size=(blocksize))
                         Criteria_ndim = self.density/density_max
                         Criteria = []
                         for point in Unthinned.T:
                             Criteria.append(Criteria_ndim[tuple(point)])
-                        Thinned = np.vstack((Thinned.astype('int'), Unthinned[:, U < Criteria].T))
+                        Thinned = np.vstack((Thinned.astype('int'),
+                                             Unthinned[:, U < Criteria].T))
                 return(Thinned[1:n+1, :])
         else:
             raise ValueError("Must provide arguments bounds and n.")
@@ -104,18 +123,11 @@ class SpatialPointProcess(PoissonProcess):
         """Generate a realization.
 
         The number of samples ``n`` and the spatial ``bounds`` must be provided.
-        The number of dimensions is taken to be the number of bound pairs in    
+        The number of dimensions is taken to be the number of bound pairs in
         ``bounds``
 
         :param int n: the number of arrivals to simulate
         :param tuple bounds: the bounds in which to generate arrivals
         :param str algo: the general algorithm
         """
-        print('a')
-        if callable(self.density):
-            try:
-                print(*bounds)
-                self.density(*bounds, **self.density_kwargs)
-            except:
-                pass
         return self._sample_spatial_point_process(n, bounds, algo, blocksize)
